680c768aee03fdbd58a85ef1bbb36164a71fc755
stas <chousetheone@gmail.com>
2024-09-30 20:05:10 +0300

    Добавил регистрацию

---
+++ user/api/v1/views.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from user.api.v1.serializers import RegisterSerializer
+
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        User.objects.create_user(
+            username=serializer.validated_data['username'],
+            email=serializer.validated_data['email'],
+            password=serializer.validated_data['password']
+        )
+
+        return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
